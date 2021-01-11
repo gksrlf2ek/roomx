6914e4f6be1709785b1a5162d809a3e9ccb51fb6
gksrlf2ek <gksrlf2ek@icloud.com>
2021-01-12 03:43:27 +0900

    Remove resizing canvas. It is better to show user full resolution canvas than resized. Testing gameloop is included. I think there should be kind of components for game. (OOP)

--- public/game.js
+++ public/game.js
@@ -1,14 +1,6 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-// resize canvas according to window size
-canvas.setAttribute("width", document.body.offsetWidth);
-canvas.setAttribute("height", document.body.offsetHeight);
-addEventListener("resize", (e) => {
-    canvas.setAttribute("width", document.body.offsetWidth);
-    canvas.setAttribute("height", document.body.offsetHeight);
-});
-
 // mouse position
 var mouse = { x: 0, y: 0, down: false };
 addEventListener("mousemove", (e) => {
@@ -22,12 +14,68 @@ addEventListener("mouseup", (e) => {
     mouse.down = false;
 });
 
+// variables for game
+stage = "title";
+
+// setting for ctx
+ctx.textAlign = "center";
+
 // game loop
-function update() {
+function draw() {
+    // clear whole canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (mouse.down) {
-        // for test
-        ctx.fillRect(mouse.x - 5, mouse.y - 5, 10, 10);
+
+    if (stage === "title") {
+        // fill whole canvas with black
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // draw title at center
+        ctx.font = "50px serif";
+        ctx.fillStyle = "white";
+        ctx.fillText("Room:X", 450, 100);
+
+        // draw start button
+        if (
+            mouse.x - 300 > 0 &&
+            mouse.x - 600 < 0 &&
+            mouse.y - 200 > 0 &&
+            mouse.y - 250 < 0
+        ) {
+            var backgroundColor = "#222222";
+            var textColor = "white";
+        } else {
+            var backgroundColor = "white";
+            var textColor = "black";
+        }
+        ctx.fillStyle = backgroundColor;
+        ctx.fillRect(300, 200, 300, 50);
+        ctx.fillStyle = textColor;
+        ctx.font = "25px serif";
+        ctx.fillText("Start", 450, 235);
+    } else if (stage === "game") {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "white";
+        ctx.font = "50px serif";
+        ctx.fillText("Game stage", 450, 100);
     }
 }
-var updateInterval = setInterval(update, 15);
+
+function update() {
+    if (
+        mouse.x - 300 > 0 &&
+        mouse.x - 600 < 0 &&
+        mouse.y - 200 > 0 &&
+        mouse.y - 250 < 0 &&
+        mouse.down
+    ) {
+        stage = "game";
+    }
+}
+
+var gameloop = () => {
+    update();
+    draw();
+};
+var interval = setInterval(gameloop, 15);
