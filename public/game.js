0da910c55018f5852eac0dc36a7e77d40e4b0458
gksrlf2ek <gksrlf2ek@icloud.com>
2021-01-12 01:34:05 +0900

    Add mouse move/up/down listener. Game system need this. There is a test code that should be removed for further use.

--- public/game.js
+++ public/game.js
@@ -9,8 +9,25 @@ addEventListener("resize", (e) => {
     canvas.setAttribute("height", document.body.offsetHeight);
 });
 
+// mouse position
+var mouse = { x: 0, y: 0, down: false };
+addEventListener("mousemove", (e) => {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+});
+addEventListener("mousedown", (e) => {
+    mouse.down = true;
+});
+addEventListener("mouseup", (e) => {
+    mouse.down = false;
+});
+
 // game loop
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (mouse.down) {
+        // for test
+        ctx.fillRect(mouse.x - 5, mouse.y - 5, 10, 10);
+    }
 }
 var updateInterval = setInterval(update, 15);
